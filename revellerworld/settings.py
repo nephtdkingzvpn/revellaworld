4b7413c552403a709cd1405b269724523327a7d6
NephygTdkingz <tdkingzict@gmail.com>
2024-12-27 13:23:16 +0100

    turned off debug

--- revellerworld/settings.py
+++ revellerworld/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost', 'revellaworldexpress.com', 'www.revellaworldexpress.com']
 
